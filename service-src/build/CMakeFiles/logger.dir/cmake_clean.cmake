f5ebeb8f5e5ed2b6086571f4987d7837e4c642e3
ubuntu <ubuntu@localhost.localdomain>
2023-12-18 09:58:20 +0800

    commit generate .so test

---
+++ service-src/build/CMakeFiles/logger.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/logger.dir/logger/service_logger.cpp.o"
+  "/home/ubuntu/fengnet/cservice/liblogger.pdb"
+  "/home/ubuntu/fengnet/cservice/liblogger.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/logger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ service-src/build/CMakeFiles/snlua.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snlua.dir/snlua/service_snlua.cpp.o"
+  "/home/ubuntu/fengnet/cservice/libsnlua.pdb"
+  "/home/ubuntu/fengnet/cservice/libsnlua.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snlua.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
